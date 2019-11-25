b2f1207219ea2cbff7d5a4ae9ceeb356475330ff
unknown <valmirjr66>
2019-11-24 23:19:31 -0300

    Criação multiplexadores

--- Codigo/bloco_operacional/mux_16_bits_1X2.vhd
+++ Codigo/bloco_operacional/mux_16_bits_1X2.vhd
@@ -1,7 +1,7 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 
-ENTITY mux_16_bits_2X1 IS
+ENTITY mux_16_bits_1X2 IS
 	GENERIC (
 		DEFAULT_SIZE : INTEGER := 16
 	);
@@ -12,16 +12,16 @@ ENTITY mux_16_bits_2X1 IS
 		B_IN : IN std_logic_vector(DEFAULT_SIZE - 1 DOWNTO 0);
 		S_OUT : OUT std_logic_vector(DEFAULT_SIZE - 1 DOWNTO 0)
 	);
-END mux_16_bits_2X1;
+END mux_16_bits_1X2;
 
-ARCHITECTURE RTL OF mux_16_bits_2X1 IS
+ARCHITECTURE RTL OF mux_16_bits_1X2 IS
 BEGIN
 	PROCESS (control, A_IN, B_IN)
 	BEGIN
 		IF (control)
 			S_OUT <= A_IN;
 		ELSE
-			S_OUT <= A_IN;
+			S_OUT <= B_IN;
 		END IF;
 	END PROCESS;
 END RTL;
---
+++ Codigo/bloco_operacional/mux_16_bits_2X4.vhd
@@ -0,0 +1,34 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+ENTITY mux_16_bits_2X4 IS
+	GENERIC (
+		DEFAULT_SIZE : INTEGER := 16
+	);
+
+	PORT (
+		control1 : IN std_logic := 0;
+		control2 : IN std_logic := 0;
+		A_IN : IN std_logic_vector(DEFAULT_SIZE - 1 DOWNTO 0);
+		B_IN : IN std_logic_vector(DEFAULT_SIZE - 1 DOWNTO 0);
+		C_IN : IN std_logic_vector(DEFAULT_SIZE - 1 DOWNTO 0);
+		D_IN : IN std_logic_vector(DEFAULT_SIZE - 1 DOWNTO 0);
+		S_OUT : OUT std_logic_vector(DEFAULT_SIZE - 1 DOWNTO 0)
+	);
+END mux_16_bits_2X4;
+
+ARCHITECTURE RTL OF mux_16_bits_2X4 IS
+BEGIN
+	PROCESS (control1, control2, A_IN, B_IN, C_IN, D_IN)
+	BEGIN
+		IF (NOT(control1) AND NOT(control2))
+			S_OUT <= A_IN;
+		ELSIF (NOT(control1) AND control2)
+			S_OUT <= B_IN;
+		ELSIF (control1 AND NOT(control2))
+			S_OUT <= C_IN;
+		ELSE
+			S_OUT <= D_IN;
+		END IF;
+	END PROCESS;
+END RTL;
